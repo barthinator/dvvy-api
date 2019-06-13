27f460c8e6b529240b18501c400af40d46bbb726
dbartho3 <davidit.bartholomew@ejgallo.com>
2019-06-13 11:27:38 -0700

    Adds basic user create and get

---
+++ user/create-user.js
@@ -0,0 +1,24 @@
+import * as dynamoDbLib from "./../libs/dynamodb-lib";
+import { success, failure } from "./../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "user",
+    Item: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      username: data.username,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
